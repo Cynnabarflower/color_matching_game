73d60f656c7e5c28e57783714bfd0a6d0ab4c5b2
Cynnabarflower <Cinnabarflower@gmail.com>
2020-08-20 01:10:34 +0300

    ggg

--- bot/app.py
+++ bot/app.py
@@ -1,15 +1,18 @@
+from flask import Flask, request
+from ftplib import FTP
+
 from flask import Flask, jsonify, request
 import time
 
-app = Flask(__name__);
+app = Flask(__name__)
 
 
 @app.route("/bot", methods=["POST"])
 def response():
-    query = dict(request.form)['query']
-    res = query + " 123 " + time.ctime()
+    name = dict(request.form).__str__()
+    res = name + " " + time.ctime()
     return jsonify({"response": res})
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port='41485')
+    app.run(host="0.0.0.0", )
